a99e33140150ece9d7d7448005deb5c95e591ad1
Owner <Owner@10.48.59.83>
2023-05-29 12:46:07 -0400

    reposit

---
+++ src/sweek3/Sweek3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sweek3;
+
+/**local rep
+ *
+ * @author Owner
+ */
+public class Sweek3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("learning github operations");// TODO code application logic here
+    }
+    
+}
